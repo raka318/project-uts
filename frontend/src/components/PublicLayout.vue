a3628fb4be3241f89e8e43a5be9217e3f32d4e25
rizki <funnehc90@gmail.com>
2026-09-17 10:07:45 +0700

    error 3

--- frontend/src/components/PublicLayout.vue
+++ frontend/src/components/PublicLayout.vue
@@ -410,16 +410,6 @@
           <!-- NOTIFICATION -->
           <button class="notification-button">
 
-            <svg viewBox="0 0 24 24">
-              <path
-                d="M18 8a6 6 0 0 0-12 0c0 7-3 7-3 9h18c0-2-3-2-3-9"
-              ></path>
-
-              <path d="M10 21h4"></path>
-            </svg>
-
-            <span class="notification-dot"></span>
-
           </button>
 
 
